0a637a27b33b2f903f18263884559ba28e776fc1
Erik Axelsson <erikaxelsson1@gmail.com>
2016-01-17 16:40:57 +0100

    and working gulp file

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp");
+var sourcemaps = require("gulp-sourcemaps");
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
+
+gulp.task("default", function () {
+  return gulp.src("src/**/*.jsx")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("all.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("build"));
+});
