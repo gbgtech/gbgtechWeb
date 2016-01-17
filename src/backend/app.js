266cbec6d1ab6f80e67d095f8904e891b33c923b
Max Witt <kirayatail@gmail.com>
2016-01-17 17:25:31 +0100

    Serving frontend content from build catalog

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/api', function (req, res) {
   res.send('Hello World!');
 });
+
+app.use(express.static('build'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
