04ff1550004e12b19d927de219a2caebd419a1a1
Johan 'Horv' Andersson <drhorv@gmail.com>
2016-01-17 16:45:58 +0100

    React hello world example

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,10 @@
-test
+var Hello = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+
+ReactDOM.render(
+  <Hello name="World" />,
+  document.getElementById('container')
+);
