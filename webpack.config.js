484dc5ef643d0059ec48bc9719a15eec7a7e2733
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-02-06 11:05:28 +0100

    Prepare webpack config for serving through express

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,12 @@
-var webpack           = require('webpack');
-var WebpackDevServer  = require('webpack-dev-server');
-var path              = require('path');
+var autoprefixer  = require('autoprefixer');
+var precss        = require('precss');
 
-var autoprefixer      = require('autoprefixer');
-var precss            = require('precss');
-
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var env = process.env.WEBPACK_ENV;
-
-var appName = 'app';
-var host = '0.0.0.0';
-var port = '9000';
-
-var plugins = [], outputFile;
-
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = appName + '.min.js';
-} else {
-  outputFile = appName + '.js';
-}
-
-var config = {
+module.exports = {
   entry: './src/frontend/index.jsx',
   devtool: 'source-map',
   output: {
-    path: __dirname + '/build',
-    filename: outputFile,
-    publicPath: __dirname + '/src/frontend'
+    path: __dirname + '/public',
+    publicPath: 'public/'
   },
   module: {
     loaders: [
@@ -39,37 +18,16 @@ var config = {
       {
         test:   /\.css$/,
         loader: 'style-loader!css-loader!postcss-loader'
-      }/*,
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: "eslint-loader",
-        exclude: /node_modules/
-      }*/
+      }
     ]
   },
   postcss: function() {
     return [autoprefixer, precss];
   },
   resolve: {
-    root: path.resolve('./src'),
     extensions: ['', '.js', '.jsx']
   },
-  plugins: plugins
+  devServer: {
+    contentBase: './public'
+  }
 };
-
-if (env === 'dev') {
-  new WebpackDevServer(webpack(config), {
-    contentBase: './src/frontend',
-    hot: true,
-    debug: true
-  }).listen(port, host, function (err, result) {
-    if (err) {
-      console.log(err);
-    }
-  });
-  console.log('-------------------------');
-  console.log('Local web server runs at http://' + host + ':' + port);
-  console.log('-------------------------');
-}
-
-module.exports = config;
