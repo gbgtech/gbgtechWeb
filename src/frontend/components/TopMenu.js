81414134abd2d5553830fdf031f44887aacc17b2
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-02-21 00:09:38 +0100

    Login box to generate email request

--- src/frontend/components/TopMenu.js
+++ src/frontend/components/TopMenu.js
@@ -1,17 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { postJson } from '../fetcher';
 
 var TopMenu = React.createClass({
-
+    getInitialState() {
+        return {
+            loginBoxOpen: false,
+            complete: false,
+            success: false
+        };
+    },
+    openLoginBox() {
+        this.setState({
+            loginBoxOpen: true,
+            complete: false
+        })
+    },
+    closeLoginBox() {
+        this.setState({
+            loginBoxOpen: false,
+            complete: false
+        })
+    },
+    handleSubmit(event) {
+        event.preventDefault();
+        postJson('/auth/email/request', { email: this.mail.value })
+        .then(status => {
+            if (status.exists) {
+                this.setState({
+                    failed: false,
+                    complete: true
+                });
+            }
+        });
+    },
     render() {
+        const { loginBoxOpen, complete } = this.state;
+
          return (
-            <ul className="top-menu">
-                <li className="logo"></li>
-                <li><Link to={'/'}>news</Link></li>
-                <li><Link to={'/hubs'}>hubs</Link></li>
-                <li><Link to={'/post'}>post</Link></li>
-                <li><a href="http://gothenburgstartup.com">gothenburg startup map</a></li>
-            </ul>
+            <nav>
+                <div className="logo"></div>
+                <ul className="navigation">
+                    <li><Link to={'/'}>news</Link></li>
+                    <li><Link to={'/hubs'}>hubs</Link></li>
+                    <li><Link to={'/post'}>post</Link></li>
+                    <li><a href="http://gothenburgstartup.com">gothenburg startup map</a></li>
+                </ul>
+                <ul className="login-menu">
+                    <li onClick={this.openLoginBox}>Login</li>
+                </ul>
+                {loginBoxOpen && (
+                    <div className="shade" onClick={this.closeLoginBox}>
+                        <div className="login-box">
+                        {complete ? (
+                            "Thanks! Check your mailbox!"
+                        ) : (
+                            <form onSubmit={this.handleSubmit} onClick={e => e.stopPropagation()}>
+                                <label>Email: <input type="email" ref={mail => this.mail = mail} placeholder="enter email"/></label>
+                                <button className="button">Submit</button>
+                            </form>
+                        )}
+                        </div>
+                    </div>
+                )}
+            </nav>
         );
     }
 });
