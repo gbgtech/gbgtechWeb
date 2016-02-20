ded6e21bb16c4e410dd81e526941b365df0dc692
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-02-20 23:01:29 +0100

    Send email on signin

--- src/backend/integration/email.js
+++ src/backend/integration/email.js
@@ -1,40 +1,88 @@
-const FormData = require('form-data');
-const fetch = require('node-fetch');
-const config = require('../config/config.js');
+'use strict';
 
-const url = config.mail.url;
-const key = config.mail.key;
+const FormData = require('form-data');
+const fetch    = require('node-fetch');
+const ejs      = require('ejs');
+const _        = require('lodash');
+const config   = require('../config/config.js');
+
+const BASE_URL = 'http://localhost:3000';
+const MAILGUN_URL = config.mail.url;
+const API_KEY     = config.mail.key;
+const AUTH_HEADER ='basic ' + new Buffer('api:' + API_KEY).toString('base64');
+
+const convertToFormData = (obj) => {
+  return Object.keys(obj).reduce((formData, key) => {
+    formData.append(key, obj[key]);
+    return formData;
+  }, new FormData());
+}
 
 function send(options) {
-  var form = new FormData();
 
   if(options.to && options.subject && options.body) {
 
-    var to = options.to.join(', ');
+    let to = options.to;
 
-    var auth ='basic ' + new Buffer('api:'+key).toString('base64');
-
-    form.append('from', options.from || "tech@gbgtech.co");
-    form.append('to', to);
-    form.append('subject', options.subject);
-    if(options.isHTML) {
-      form.append('html', options.body);
-    } else {
-      form.append('text', options.body);
+    if (Array.isArray(options.to)) {
+      to = to.join(', ');
     }
 
-    fetch(url, {method: 'POST', body: form, headers: {Authorization: auth}}).then(
-      function(res) {
-        return res.json();
+    const formParams = {
+      from: options.from || 'tech@gbgtech.co',
+      to,
+      subject: options.subject
+    };
+
+    if (options.isHTML) {
+      formParams.html = options.body;
+    } else {
+      formParams.text = options.body;
+    }
+
+    const form = convertToFormData(formParams);
+
+    return fetch(MAILGUN_URL, {
+      method: 'POST',
+      body: form,
+      headers: {
+        Authorization: AUTH_HEADER
       }
-    ).then(function(json) {
-      console.log(json);
     })
+    .then(res => res.json())
+    .then(json => console.log(json));
+
   } else {
     console.error("Email service missing required fields");
   }
 }
 
+function renderTemplate(filename, params) {
+  return ejs.render('<%- include(\'../template/base.ejs\', params) %>',
+    {template: filename,
+      params: _.assign({}, params, {
+        base_url: BASE_URL
+      })
+    },
+    {filename: __filename }
+  );
+}
+
+function sendSigninMail(to, url) {
+  const body = renderTemplate('../template/signin.ejs', {
+    url
+  });
+
+  return send({
+    to,
+    body,
+    isHTML: true,
+    subject: '[#GBGtech] Your sign in link'
+  });
+}
+
+
 module.exports = {
-  send: send
+  send,
+  sendSigninMail
 };
