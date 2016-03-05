aaf8b40168cba88a849031e4048b2ed3f9bb234c
Erik Axelsson <erikaxelsson1@gmail.com>
2016-03-05 12:21:45 +0100

    and add reddit files

---
+++ src/backend/outlet/reddit.js
@@ -0,0 +1,31 @@
+var rawjs = require("raw.js");
+const config = require('../config/config');
+
+
+module.exports = {
+    postEvents
+};
+
+
+
+
+function postEvents(aPost) {
+    var reddit = new rawjs("raw.js example script");
+
+    reddit.setupOAuth2(config.reddit.oath2User, config.reddit.oath2Key);
+    console.log(aPost);
+
+    reddit.auth({"username": config.reddit.user, "password": config.reddit.pass}, function(err, response) {
+      console.log(aPost);
+        if(err) {
+            console.log("Unable to authenticate user: " + err);
+        } else {
+          var post={r:"gbgtech",url:"gbgtech.se/news/"+aPost.slug,title:aPost.title};
+
+          reddit.submit(post,function(err,id) {
+            console.log(err);
+            console.log(id);
+          });
+        }
+    });
+}
