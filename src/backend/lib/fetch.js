7a636610588e9bab08d3270e9bf612d893c618f6
Max Witt <kirayatail@gmail.com>
2016-03-05 12:56:28 +0100

    Fix email integration issues

---
+++ src/backend/lib/fetch.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+
+function checkStatus(response) {
+    console.log(response);
+    if (response.status >= 200 && response.status < 300) {
+        return response
+    } else {
+        var error = new Error(response.statusText);
+        error.response = response;
+        throw error
+    }
+}
+
+module.exports = (url, args) =>
+  fetch(url, args)
+  .then(checkStatus);
