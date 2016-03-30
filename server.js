d5ee62cb4a735854104b629f6984a9eb23648866
Erik Axelsson <erikaxelsson1@gmail.com>
2016-03-30 22:36:26 +0200

    and adding the .eslintrc file

--- server.js
+++ server.js
@@ -2,31 +2,13 @@ require('dotenv').config();
 var express = require('express');
 var webpack = require('webpack');
 
-
-
-//var webpackConfig = require('./webpack.config');
-
 var port = process.env.PORT || 3001;
 
 var app = express();
 
 
 require('./src/backend/config/express.js')(app);
-/*
-if (true) { // Assumes that this runs as dev. Fix and add production routine
-  var compiler = webpack(webpackConfig);
 
-  app.use(webpackMiddleware(compiler, {
-      publicPath: '/build',
-      stats: {
-          colors: true,
-          chunks: false
-      }
-  }));
-
-  app.use(webpackHotMiddleware(compiler));
-}
-*/
 app.listen({port: port, bind: '0.0.0.0'}, () => {
   console.log("Backend is on port "+ port);
 });
