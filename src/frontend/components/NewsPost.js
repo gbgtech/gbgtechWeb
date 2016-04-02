d7fd05c191f8622ea222fcf5af14f857d66225b4
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-04-03 00:07:55 +0200

    Link to edit post, closes #31

--- src/frontend/components/NewsPost.js
+++ src/frontend/components/NewsPost.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
 import { formatDate } from '../formatter';
+import { roles } from '../roles';
 
-const NewsPost = ({ post }) => (
+const userCanEditPost = (user, post) => {
+  if (!user) { return false; }
+  return (user._id === post.author._id) || user.role >= roles.admin;
+};
+
+const NewsPost = ({ post, user }) => (
     <article>
         <header>
             {post.accepted === 'WAITING' && <div>POST UNDER REVIEW (NOT PUBLISHED YET)</div>}
             {post.accepted === 'DENIED' && <div>POST DENIED (NOT PUBLISHED)</div>}
             <h2><Link to={`/news/${post.slug}`}>{post.title}</Link></h2>
             {post.origin && <ProviderBadge {...post.origin} />}
+            {userCanEditPost(user, post) && <Link to={`/news/${post.slug}/edit`}>Edit post</Link>}
         </header>
         <small>by {post.author && post.author.email} | {post.createdAt} | {post.categories.map(c => c.name).join(', ')}</small>
         <p dangerouslySetInnerHTML={{__html: post.body}}></p>
@@ -34,4 +42,8 @@ const GoogleMapsLink = ({ lat, lng, name }) => (
     <a href={`https://www.google.com/maps/preview/@${lat},${lng},13z`} target="_blank">{name}</a>
 );
 
-export default NewsPost;
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+export default connect(mapStateToProps)(NewsPost);
---
+++ src/frontend/roles.js
@@ -0,0 +1,6 @@
+export const roles = {
+  admin: 400,
+  editor: 300,
+  author: 200,
+  user: 100
+};
