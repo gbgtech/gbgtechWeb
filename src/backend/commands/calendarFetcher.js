df9e8a8dbbf594d5d945b2196a3748917875e18d
Johan 'Horv' Andersson <drhorv@gmail.com>
2016-04-17 22:22:19 +0200

    Added command for Google Calendar outlet

--- src/backend/commands/calendarFetcher.js
+++ src/backend/commands/calendarFetcher.js
@@ -4,45 +4,45 @@ var mongoose = require('mongoose');
 var config = require('../config/config');
 var Posts = mongoose.model('Posts');
 var _ = require('lodash');
+var promiseRetry = require('promise-retry');
+
+function backoffExp(promiseFunc, successCallback) {
+  return promiseRetry((retry, number) => {
+    return promiseFunc().catch(retry);
+  });
+}
+
+const outletInfoFromEvent = (event) => ({ 
+            name: "googlecalendar", 
+            id: event.id,
+            url: event.url
+          })
+
+const onlyNonGoogleCalendarOutlets = p => !_(p.outlets).some({name: 'googlecalendar'})
+
+const updatePostWithGoogleCalendarOutlet = (post, event) => {
+  post.outlets.push(outletInfoFromEvent(event));
+  return Bacon.fromNodeCallback(post.save);
+}
 
 Bacon.fromNodeCallback(mongoose, 'connect', config.db).onValue(() => {
   // Load Mongoose models
-  var calender = require('../outlet/googlecalendar');
+  var calendar = require('../outlet/googlecalendar');
 
-
-  Posts.find().then(posts =>{
-    console.log("before",posts.length);
-    posts=posts.filter(p => !_(p.outlets).some({name: 'googlecalendar'}))
-    console.log("after",posts.length);
-    posts.map((post,index) =>{
-      setTimeout(()=>{
-        calender.postEvents(post).then(event=>{
-          var outletInfo={name:"googlecalendar",id:event.id,url:event.url};
-          post.outlets.push(outletInfo)
-          post.save();
-        }).catch(err=>console.log("error: ",err));
-      },1000*index);
+  baconStream = Bacon.fromPromise(Posts.find({ eventData: { $ne: null } }))
+    .flatMap(Bacon.fromArray)
+    .log('before')
+    .filter(onlyNonGoogleCalendarOutlets)
+    .log('after')
+    .flatMap((post) => { 
+      const bs = Bacon
+      .fromPromise(backoffExp(() => calendar.postEvents(post)))
+      .flatMap(event => updatePostWithGoogleCalendarOutlet(post, event));
+      bs.onError(err => console.error('Failed to post calendar event', err))
+      return bs 
     });
 
-  })
+  baconStream.onError((err) => console.log("Something went terribly wrong", err));
+  baconStream.onEnd(() => { console.log('Finished'); mongoose.connection.close();});
+
 })
-
-/*  Posts.find({outlets:{
-
-$nin:
-[{
-name: "googlecalendar"
-}]
-}}).then(posts =>{
-
-posts.map(post =>{
-calender.postEvents(post).then(event=>{
-
-var outletInfo={name:"googlecalendar",id:event.id,url:event.url};
-post.outlets.push(outletInfo)
-post.save();
-}).catch(err=>console.log("error: ",err));
-
-});
-
-});*/
