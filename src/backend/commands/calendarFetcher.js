252c3d43c13b90d3fa35c683c3b314248bfe70b5
Erik Axelsson <erikaxelsson1@gmail.com>
2016-04-24 16:20:54 +0200

    Remove all calendars

--- src/backend/commands/calendarFetcher.js
+++ src/backend/commands/calendarFetcher.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var commands = require('./commands');
 const Bacon = require('baconjs').Bacon;
 var mongoose = require('mongoose');
@@ -5,13 +7,13 @@ var config = require('../config/config');
 var Posts = mongoose.model('Posts');
 const _ = require('lodash');
 
-const outletInfoFromEvent = (event) => ({ 
-  name: "googlecalendar", 
+const outletInfoFromEvent = (event) => ({
+  name: "googlecalendar",
   id: event.id,
   url: event.url
 })
 
-const onlyNonGoogleCalendarOutlets = p => !_(p.outlets).some({name: 'googlecalendar'})
+
 
 const updatePostWithGoogleCalendarOutlet = (event, post) => {
   post.outlets.push(outletInfoFromEvent(event));
@@ -22,20 +24,29 @@ Bacon.fromNodeCallback(mongoose, 'connect', config.db).onValue(() => {
   // Load Mongoose models
   var calendar = require('../outlet/googlecalendar');
 
+  let currentFilter = p => !_(p.outlets).some({name: 'googlecalendar'});
+
+  if(process.argv[2]=="--resset"){
+    console.log("resseting");
+    currentFilter = () => true;
+    calendar.resetAll();
+  }
+
+/*
   const postStream = Bacon.fromPromise(Posts.find({ eventData: { $ne: null } }))
-    .flatMap(Bacon.fromArray)
-    .filter(onlyNonGoogleCalendarOutlets)
-    .doAction(post => console.log('postStream', post._id));
+  .flatMap(Bacon.fromArray)
+  .filter(currentFilter)
+  .doAction(post => console.log('postStream', post._id));
 
   calendar.postEventsFromStream(postStream)
-    .doError(err => console.error('Failed to post calendar event', err))
-    .combine(postStream, updatePostWithGoogleCalendarOutlet)
-    .flatMap(s => s)
-    .log()
-    .doError(err => console.log("Something went terribly wrong", err))
-    .onEnd(() => {
-      console.log('Finished');
-      mongoose.connection.close();
-    });
-
+  .doError(err => console.error('Failed to post calendar event', err))
+  .combine(postStream, updatePostWithGoogleCalendarOutlet)
+  .flatMap(s => s)
+  .log()
+  .doError(err => console.log("Something went terribly wrong", err))
+  .onEnd(() => {
+    console.log('Finished');
+    mongoose.connection.close();
+  });
+  */
 });
--- src/backend/outlet/googlecalendar.js
+++ src/backend/outlet/googlecalendar.js
@@ -1,3 +1,4 @@
+"use strict";
 const config = require('../config/config');
 
 const Bacon = require('baconjs').Bacon;
@@ -7,7 +8,8 @@ const auth = config.googlecalendar.apiKey;
 
 module.exports = {
   postEvent,
-  postEventsFromStream
+  postEventsFromStream,
+  resetAll
 };
 
 const createJWTClient = () => new google.auth.JWT(
@@ -48,16 +50,18 @@ const googleCalendarInsert = (event, jwtClient) => new Promise((resolve, reject)
 });
 
 const authorizeJWTClient = (client) => new Promise((resolve, reject) =>
-  client.authorize(err => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(client);
-    }
-  }));
+client.authorize(err => {
+  if (err) {
+    reject(err);
+  } else {
+    resolve(client);
+  }
+}));
 
-const baconRetry = (client, event) => Bacon.retry({
-  source: () => Bacon.fromPromise(googleCalendarInsert(event, client)),
+//googleCalendarInsert(event, client)
+
+const baconRetry = (f, client, event) => Bacon.retry({
+  source: () => Bacon.fromPromise(f(event, client)),
   retries: 10,
   delay: (ctx) => Math.pow(2, ctx.retriesDone) + Math.random() * 1000
 });
@@ -67,13 +71,75 @@ function postEventsFromStream(eventStream) {
 
 
   return Bacon.fromPromise(authorizeJWTClient(jwtClient)).toProperty()
-    .combine(
-      eventStream.map(createGoogleCalendarEventFromPost),
-      baconRetry
-    )
-    .flatMap(s => s)
+  .combine(
+    eventStream.map(createGoogleCalendarEventFromPost),
+    baconRetry.bind(this, googleCalendarInsert)
+  )
+  .flatMap(s => s)
 }
 
+const deleteEvent = (event, jwtClient) => new Promise((resolve, reject) => {
+  calendar.events.delete({
+    auth: jwtClient,
+    calendarId: config.googlecalendar.calendarId,
+    eventId: event.id
+  }, (err, res) => {
+    if (err) {
+//      console.log('There was an error contacting the Calendar when deleting: ', err);
+      reject(err);
+    } else {
+      resolve(res===undefined);
+    }
+  });
+});
+
+function resetAll() {
+  const jwtClient = createJWTClient();
+  calendar.events.list({
+    auth: jwtClient,
+    calendarId: config.googlecalendar.calendarId
+  }, (err, res) => {
+    if (err) {
+      console.log('There was an error contacting the Calendar service: ', err);
+    } else {
+      var events = res.items;
+    //  events=res.items.slice(0,67);
+      console.log(events);
+      var removeObj=function(arrayObj){
+        if(arrayObj.length==0){
+          return;
+        }
+        let event=arrayObj.pop();
+        var retriesDone=0;
+        //remove events
+        var stream = Bacon.retry({
+          source: () => Bacon.fromPromise(deleteEvent(event, jwtClient)),
+          retries: 50,
+          delay: (ctx) => {
+            retriesDone++;
+            console.log("backoff: ",retriesDone);
+            return  Math.pow(2, retriesDone) + Math.random() * 1000}
+        });
+
+        stream.subscribe(res => {
+          console.log(res,retriesDone);
+          if(retriesDone!=0 && res){
+            retriesDone--;
+          }
+          removeObj(arrayObj)
+        });
+      }
+      removeObj(events);
+
+
+  /*    for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var stream = baconRetry(deleteEvent, jwtClient, event);
+        stream.subscribe(console.log);
+      }*/
+    }
+  });
+}
 
 function postEvent(post) {
   const jwtClient = createJWTClient();
@@ -81,7 +147,7 @@ function postEvent(post) {
   console.log(jwtClient);
 
   return Bacon.fromNodeCallback(jwtClient.authorize)
-    .map(() => jwtClient)
-    .flatMap(client => googleCalendarInsert(post, client))
-    .toPromise();
+  .map(() => jwtClient)
+  .flatMap(client => googleCalendarInsert(post, client))
+  .toPromise();
 }
