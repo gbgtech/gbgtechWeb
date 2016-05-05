ea250e711749b499e053cf93c8bab52bb873526b
André Samuelsson <andresamuelsson94@gmail.com>
2016-05-05 22:49:11 +0200

    Email command implemented. Mail posts to subscribing users.

---
+++ src/backend/commands/emailProvider.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const commands = require('./commands');
+const Bacon = require('baconjs').Bacon;
+const mongoose = require('mongoose');
+const Posts = mongoose.model('Posts');
+const config = require('../config/config');
+
+const _ = require('lodash');
+
+
+let currentFilter = p => !_(p.outlets).some({name: 'email'});
+
+Bacon.fromNodeCallback(mongoose, 'connect', config.db).onValue(() => {
+
+  var email = require('../outlet/email');
+
+  const postStream = Posts.find({ eventData: { $ne: null },accepted:"APPROVED" }).then(res=>{
+    email.sendToSubscribers(res.filter(currentFilter)).then(()=>{console.log("close mongose");mongoose.connection.close()})
+  });
+});
