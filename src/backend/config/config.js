29a3e74fad9162dd59686b529a45b33b29e70bb8
Erik Axelsson <erikaxelsson1+git@gmail.com>
2016-05-06 23:19:43 +0200

    change mongo db url

--- src/backend/config/config.js
+++ src/backend/config/config.js
@@ -9,7 +9,7 @@ const DATABASE_NAME = '/gbgtechweb';
 
 module.exports = {
   db: process.env.MONGOHQ_URL
-			|| process.env.MONGOLAB_URI
+			|| process.env.MONGODB_URI
 			|| 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR
 			|| 'localhost') + '/gbgtechweb',
 	sessionCookie: {
