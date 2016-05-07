b0e97a5e315577cabdcf7469313f60e6d418e40e
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-05-07 22:51:39 +0200

    Fetch posts in redux, with thunk

--- src/frontend/store/actions.js
+++ src/frontend/store/actions.js
@@ -3,6 +3,13 @@ import { get } from '../fetcher';
 export const SET_SIGNED_IN = 'SET_SIGNED_IN';
 export const SET_SIGNED_OUT = 'SET_SIGNED_OUT';
 
+export const REQUEST_POST = 'REQUEST_POST';
+export const RECEIVE_POST = 'RECEIVE_POST';
+export const RECEIVE_POST_ERROR = 'RECEIVE_POST_ERROR';
+
+export const REQUEST_POSTS = 'REQUEST_POSTS';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const RECEIVE_POSTS_ERROR = 'RECEIVE_POSTS_ERROR';
 
 export const setSignedIn = (user) => ({
   type: SET_SIGNED_IN,
@@ -15,3 +22,39 @@ export const setSignedOut = () => {
     type: SET_SIGNED_OUT
   });
 };
+
+export const requestPosts = () => {
+  return dispatch => {
+    get('/posts')
+      .then(posts => dispatch(receivePosts(posts)))
+      .catch(error => dispatch(receivePostsError(error)))
+  }
+);
+
+export const receivePosts = (posts) => ({
+  type: RECEIVE_POSTS,
+  posts
+});
+
+export const receivePostsError = (error) => ({
+  type: RECEIVE_POSTS_ERROR,
+  error
+});
+
+export const requestPost = (id) => {
+  return dispatch => {
+    get(`/posts/${id}`)
+      .then(post => dispatch(receivePost(post)))
+      .catch(error => dispatch(receivePostError(error)))
+  }
+};
+
+export const receivePost = (post) => ({
+  type: RECEIVE_POST,
+  post
+});
+
+export const receivePostError = (error) => ({
+  type: RECEIVE_POST_ERROR,
+  error
+});
