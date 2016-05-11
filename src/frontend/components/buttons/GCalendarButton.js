ba52f8222c466505bba27184416363a0cde48693
Erik Axelsson <erikaxelsson1+git@gmail.com>
2016-05-11 23:10:45 +0200

    try to hardcode gcall

--- src/frontend/components/buttons/GCalendarButton.js
+++ src/frontend/components/buttons/GCalendarButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GoogleCalendarButton = () => {
   const urlPrefix = "https://calendar.google.com/calendar/embed?src=";
-  const googleCalendarId = GOOGLECALENDAR_ID;
+  const googleCalendarId = "qvrnclm8e6ndipoki2hh6rtg1k@group.calendar.google.com&ctz=Europe/Stockholm#main_7";
 
   return (
     <a href={urlPrefix + googleCalendarId} className="main-follow-button calendar-btn" target="_blank" rel="noopener noreferrer" />
